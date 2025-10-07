23e2bdfb2ece4a5eb18cb56eec6eaf9d642af39b
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-10-07 16:20:57 +0530

    create auth context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, db } from "../firebase/config";
+import { doc, getDoc } from "firebase/firestore";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const isLoggedIn = user ? true : false;
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      if (currentUser) {
+        const docSnap = await getDoc(doc(db, "users", currentUser?.uid));
+        if (docSnap.exists()) {
+          setUser({ ...currentUser, ...docSnap.data() });
+        } else {
+          setUser(null);
+        }
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+  return (
+    <AuthContext.Provider value={{ user, isLoggedIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
